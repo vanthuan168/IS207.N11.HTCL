8f3dbfac12fc17c241806f59cf52ee0bd050f79a
ThuanBui <buivanthuan1608@gmail.com>
2022-10-31 01:14:52 +0700

    header first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+// function openTab(evt, TabName) {
+//   var i, tabcontent, tablinks, temp;
+//   tabcontent = document.getElementsByClassName("subnav-product__tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("nav-item__product--tab-link");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+  
+//   document.getElementById(TabName).style.display = "flex";
+//   evt.currentTarget.className += " active";
+//   // alert(evt.currentTarget.className)
+// }
+
